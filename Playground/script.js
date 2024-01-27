40ab2af5c1f5d609269364a8999566052d0edf50
Guilherme Drumond <96558039+Guilherme-Drumond@users.noreply.github.com>
2024-01-27 15:22:38 -0300

    Calculadora Financeira

---
+++ Playground/script.js
@@ -0,0 +1,17 @@
+function calcular(){
+
+    const meses = 12;
+    const tempo = document.querySelector('#anos').value;
+    const valor = document.querySelector('#digito').value;
+    const formula = (valor/(meses*tempo)).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+
+    const valorf = (valor).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+
+
+    if(isNaN(tempo) || isNaN(valor) || tempo <= 0){
+        alert('Por favor, insira valores válidos.')
+        return;
+    }
+
+    document.querySelector('#resultado').innerHTML = "Você deve juntar "+formula+" por mês durante "+tempo+" Anos Para ter "+valorf+" Reais";
+}
